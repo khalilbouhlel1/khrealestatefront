b652a1a993fe933a9d8d252c70e66d70c6cf4a6c
khalilbouhlel1 <bouhlel@isikef.u-jendouba.tn>
2024-11-20 16:25:19 +0100

    updated 2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
   build: {
     rollupOptions: {
-      external: ['gsap']
-    }
-  }
+      external: ['gsap'],
+      output: {
+        format: 'es',
+      },
+    },
+  },
 })
